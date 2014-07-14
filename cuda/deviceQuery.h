fb46733b944f4ba62f80351eda6900251c897a39
HarryLong <hjlong1@gmail.com>
2014-07-14 20:07:07 +0200

    Improved timing + output of path is now essential

--- cuda/deviceQuery.h
+++
@@ -1,9 +0,0 @@
-#ifndef DEVICE_QUERY_H
-#define DEVICE_QUERY_H
-
-#include "cudaHeader.h"
-
-int getValFromSMVer(int major, int minor, SMVal* vals);
-int printDeviceInfo();
-
-#endif //DEVICE_QUERY_H 
